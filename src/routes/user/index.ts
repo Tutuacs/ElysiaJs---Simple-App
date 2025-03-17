26506fdd837539890c0c67fca6d40f0068158142
Arthur_Silva <arthursilva.rs1@gmail.com>
2025-03-17 16:45:23 -0300

    :sparkles: feat: add guard on UserHandler

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,10 +1,36 @@
-import Elysia from "elysia";
+import Elysia, { t } from "elysia";
 import { listUsersHandler } from "./controllers/list/listUsers";
 import { findByEmailUserHandler } from "./controllers/find/byEmail/findUser";
 import { findByIdUserHandler } from "./controllers/find/byId/findUser";
 
 
-export const UserHandler = new Elysia({ name: "UserHandler", prefix: "/user" })
-    .use(listUsersHandler)
-    .use(findByEmailUserHandler)
-    .use(findByIdUserHandler)
+export const UserHandler = new Elysia({ name: "UserHandler", prefix: "/user",  tags: ["User"] })
+    // .use(listUsersHandler)
+    // .use(findByEmailUserHandler)
+    // .use(findByIdUserHandler)
+    // * UseHandlers out guard only on routes without required auth
+    .guard({
+        cookie: t.Cookie({
+            auth: t.String(),
+        }, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "strict",
+        }),
+        detail: {
+            parameters: [{
+                in: 'cookie',
+                name: 'auth',
+                required: true,
+                schema: {
+                    description: "JWT token saved on cookie httpOnly on client side login",
+                    type: 'string' as const,
+                }
+            }],
+        },
+    }, (UserHandler) => 
+        UserHandler
+        .use(listUsersHandler)
+        .use(findByEmailUserHandler)
+        .use(findByIdUserHandler)
+    )
